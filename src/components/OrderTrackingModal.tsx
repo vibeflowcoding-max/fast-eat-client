ae27ea808afcbbcc92a3ab85092a2ae6a3994911
javier <javier.sanchez@ayahealthcare.com>
2026-02-24 11:15:37 -0600

    fix: order tracking modal counter-offer imports and logic

--- src/components/OrderTrackingModal.tsx
+++ src/components/OrderTrackingModal.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useOrderTracking } from '../hooks/useOrderTracking';
 import { useCartStore } from '@/store';
 import BidRow from './BidRow';
